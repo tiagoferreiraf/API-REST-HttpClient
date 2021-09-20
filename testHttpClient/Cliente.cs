4eecec1e7dff36a908978d96a717171464edf856
tiagoalfa <tiagoferreirafonsecaa@gmail.com>
2021-09-20 17:01:26 -0300

    Add project files.

---
+++ testHttpClient/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testHttpClient
+{
+    class Cliente
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string website { get; set; }
+    }
+}
---
+++ testHttpClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace testHttpClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient cliente = new HttpClient { BaseAddress = new Uri("http://jsonplaceholder.typicode.com/") };
+            var response = await cliente.GetAsync("users");
+            var content = await response.Content.ReadAsStringAsync();
+           
+            var users = JsonConvert.DeserializeObject<Cliente[]>(content);
+            
+            foreach(var item in users){
+                System.Console.WriteLine(item.website);
+            }
+        }
+    }
+}
